8dd17d23891231e2141125056a09eb6344a530e0
Andrew <andrew.kepych@gmail.com>
2021-01-23 13:48:19 -0600

    Calibration and Increased Dead-zone

--- src/main/java/com/swervedrivespecialties/exampleswerve/commands/DriveCommand.java
+++ src/main/java/com/swervedrivespecialties/exampleswerve/commands/DriveCommand.java
@@ -15,17 +15,17 @@ public class DriveCommand extends Command {
     @Override
     protected void execute() {
         double forward = -Robot.getOi().getPrimaryJoystick().getRawAxis(1);
-        forward = Utilities.deadband(forward);
+        forward = Utilities.deadband(forward, 0.1);
         // Square the forward stick
         forward = Math.copySign(Math.pow(forward, 2.0), forward);
 
         double strafe = -Robot.getOi().getPrimaryJoystick().getRawAxis(0);
-        strafe = Utilities.deadband(strafe);
+        strafe = Utilities.deadband(strafe, 0.1);
         // Square the strafe stick
         strafe = Math.copySign(Math.pow(strafe, 2.0), strafe);
 
         double rotation = -Robot.getOi().getPrimaryJoystick().getRawAxis(4);
-        rotation = Utilities.deadband(rotation);
+        rotation = Utilities.deadband(rotation, 0.1);
         // Square the rotation stick
         rotation = Math.copySign(Math.pow(rotation, 2.0), rotation);
 
